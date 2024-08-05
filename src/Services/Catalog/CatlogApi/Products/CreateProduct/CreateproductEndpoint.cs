6626d22ae48e0a2d9e0e6fec26f7c4471339e110
Dwayne Williams <dwaynewillmakeit@gmail.com>
2024-08-04 21:42:56 -0500

    Create post request endpoint

--- src/Services/Catalog/CatlogApi/Products/CreateProduct/CreateproductEndpoint.cs
+++ src/Services/Catalog/CatlogApi/Products/CreateProduct/CreateproductEndpoint.cs
@@ -1,10 +1,32 @@
-﻿namespace CatlogApi.Products.CreateProduct
-{
+﻿namespace CatlogApi.Products.CreateProduct;
     public record CreateProductRequest(string Name, string Description, List<string> Category, string ImageFile);
 
     public record CreateProductResponse(Guid id);
 
-    public class CreateproductEndpoint
+public class CreateproductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("/products", 
+            async (CreateProductRequest request, ISender sender) => 
+            {
+                var command = request.Adapt<CreateProductComand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.id}",response);
+            }
+            )
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create a product")
+            ;
+
+        throw new NotImplementedException();
     }
 }
+
