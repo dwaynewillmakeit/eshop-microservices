abc23d5265180d7c34cbd3f5b207e5fdb9a05cb7
Dwayne Williams <dwaynewillmakeit@gmail.com>
2024-08-06 21:37:13 -0500

    Get Products by category feature

---
+++ src/Services/Catalog/CatlogApi/Products/GetProductsByCategory/GetProductsByCategoryEndpoint.cs
@@ -0,0 +1,32 @@
+﻿
+using CatlogApi.Products.GetProducts;
+
+namespace CatlogApi.Products.GetProductsByCategory
+{
+
+    public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+
+    public class GetProductsByCategoryEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{category}/", async (string category, ISender sender) =>
+            {
+
+
+                var result = await sender.Send(new GetProductsByCategoryQuery(category));
+
+                var response = result.Adapt<GetProductByCategoryResponse>();
+
+                return Results.Ok(response);
+
+            })
+                 .WithName("GetProductByCategory")
+                .Produces<GetProductResponse>(StatusCodes.Status200OK)
+                .WithDescription("Get Products By Category")
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Products By Category");
+        }
+    }
+}
---
+++ src/Services/Catalog/CatlogApi/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace CatlogApi.Products.GetProductsByCategory
+{
+    public record GetProductsByCategoryQuery(String Category) : IQuery<GetProductsByCategoryResult>;
+
+    public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+    internal class GetProductsByCategoryQueryHandler(
+        IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> _logger
+        ) : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+    {
+        public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Getting products by category",query);
+
+            var products =  await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync();
+
+
+
+            return new GetProductsByCategoryResult(products);
+        }
+    }
+}
