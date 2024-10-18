86a71357c4a5b889f541ce2c0ff501ee531f65a1
Dwayne Williams <dwaynewillmakeit@gmail.com>
2024-10-18 01:13:01 -0500

    Create Dtos and Order Create Command

---
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Ordering.Application.Dtos;
+
+public record OrderItemDto(
+    Guid OrderId, Guid ProductId, int Quantity, decimal Price);
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure
 {
@@ -27,7 +28,8 @@ namespace Ordering.Infrastructure
                 }
                         
                 );
-            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>()
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
             return services;
         }
