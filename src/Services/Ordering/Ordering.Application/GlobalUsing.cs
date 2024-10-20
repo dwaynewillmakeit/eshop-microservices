6b9df631e7ea4b95f2ab3128e3480df464c01e2f
Dwayne Williams <dwaynewillmakeit@gmail.com>
2024-10-19 22:36:43 -0500

    Create the base for event handlers

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -3,3 +3,6 @@ global using Ordering.Domain.ValueObjects;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
