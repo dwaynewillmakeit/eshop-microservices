db32f4b0e90ce9d87550a2d7373843068f348810
Dwayne Williams <dwaynewillmakeit@gmail.com>
2024-10-20 23:17:10 -0500

    Initial commit for building block messaging library

---
+++ src/BuildingBlocks/BuildingBlock.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlock.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid id = Guid.NewGuid();
+    public DateTime OccuredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
